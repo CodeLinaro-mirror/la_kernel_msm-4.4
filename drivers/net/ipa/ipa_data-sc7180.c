b810c747e06ab8c7aa64c9c9c43d551687315947
Alex Elder <elder@linaro.org>
2021-02-10 10:12:04 -0600

    net: ipa: make all configuration data constant
    All of the platform configuration data should be constant, but
    that isn't the case for the memory regions, interconnects, and
    clocks.  Fix this.
    
    Signed-off-by: Alex Elder <elder@linaro.org>

--- drivers/net/ipa/ipa_data-sc7180.c
+++ drivers/net/ipa/ipa_data-sc7180.c
@@ -308,7 +308,7 @@ static const struct ipa_mem ipa_mem_local_data[] = {
 	},
 };
 
-static struct ipa_mem_data ipa_mem_data = {
+static const struct ipa_mem_data ipa_mem_data = {
 	.local_count	= ARRAY_SIZE(ipa_mem_local_data),
 	.local		= ipa_mem_local_data,
 	.imem_addr	= 0x146a8000,
@@ -318,7 +318,7 @@ static struct ipa_mem_data ipa_mem_data = {
 };
 
 /* Interconnect bandwidths are in 1000 byte/second units */
-static struct ipa_interconnect_data ipa_interconnect_data[] = {
+static const struct ipa_interconnect_data ipa_interconnect_data[] = {
 	{
 		.name			= "memory",
 		.peak_bandwidth		= 465000,	/* 465 MBps */
@@ -337,7 +337,7 @@ static struct ipa_interconnect_data ipa_interconnect_data[] = {
 	},
 };
 
-static struct ipa_clock_data ipa_clock_data = {
+static const struct ipa_clock_data ipa_clock_data = {
 	.core_clock_rate	= 100 * 1000 * 1000,	/* Hz */
 	.interconnect_count	= ARRAY_SIZE(ipa_interconnect_data),
 	.interconnect_data	= ipa_interconnect_data,
