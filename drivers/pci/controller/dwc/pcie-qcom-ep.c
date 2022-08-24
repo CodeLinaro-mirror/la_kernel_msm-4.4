362b96f0a47972213187e1360261a755f242149f
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2022-08-24 17:12:40 +0530

    PCI: qcom-ep: Add support for SM8450 SoC
    Add support for SM8450 SoC to the Qualcomm PCIe Endpoint Controller
    driver. The driver uses the same config as of the existing SDX55 chipset.
    So additional settings are not required.
    
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>

--- drivers/pci/controller/dwc/pcie-qcom-ep.c
+++ drivers/pci/controller/dwc/pcie-qcom-ep.c
@@ -789,6 +789,7 @@ static int qcom_pcie_ep_remove(struct platform_device *pdev)
 
 static const struct of_device_id qcom_pcie_ep_match[] = {
 	{ .compatible = "qcom,sdx55-pcie-ep", },
+	{ .compatible = "qcom,sm8450-pcie-ep", },
 	{ }
 };
 
