f14495366c1d924f6c468657e4df997e9e12162e
Abhishek Singh <quic_abhishes@quicinc.com>
2024-07-08 15:27:37 +0530

    msm: adsprpc: use-after-free (UAF) in global maps
    Currently, remote heap maps get added to the global list before the
    fastrpc_internal_mmap function completes the mapping. Meanwhile, the
    fastrpc_internal_munmap function accesses the map, starts unmapping, and
    frees the map before the fastrpc_internal_mmap function completes,
    resulting in a use-after-free (UAF) issue. Add the map to the list after
    the fastrpc_internal_mmap function completes the mapping
    
    Change-Id: Ia7867ff0dee913e2b02af825981bd14b92f4d84b
    Signed-off-by: Abhishek Singh <quic_abhishes@quicinc.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -443,28 +443,27 @@ static void fastrpc_remote_buf_list_free(struct fastrpc_file *fl)
 	} while (free);
 }
 
+static void fastrpc_mmap_add_global(struct fastrpc_mmap *map)
+{
+	struct fastrpc_apps *me = &gfa;
+
+	spin_lock(&me->hlock);
+	hlist_add_head(&map->hn, &me->maps);
+	spin_unlock(&me->hlock);
+}
+
 static void fastrpc_mmap_add(struct fastrpc_mmap *map)
 {
-	if (map->flags == ADSP_MMAP_HEAP_ADDR ||
-				map->flags == ADSP_MMAP_REMOTE_HEAP_ADDR) {
-		struct fastrpc_apps *me = &gfa;
+	struct fastrpc_file *fl = map->fl;
 
-		spin_lock(&me->hlock);
-		hlist_add_head(&map->hn, &me->maps);
-		spin_unlock(&me->hlock);
-	} else {
-		struct fastrpc_file *fl = map->fl;
-
-		spin_lock(&fl->hlock);
-		hlist_add_head(&map->hn, &fl->maps);
-		spin_unlock(&fl->hlock);
-	}
+	spin_lock(&fl->hlock);
+	hlist_add_head(&map->hn, &fl->maps);
+	spin_unlock(&fl->hlock);
 }
 
 static int fastrpc_mmap_find(struct fastrpc_file *fl, int fd, uintptr_t va,
 			size_t len, int mflags, struct fastrpc_mmap **ppmap)
 {
-	struct fastrpc_apps *me = &gfa;
 	struct fastrpc_mmap *match = NULL, *map = NULL;
 	struct hlist_node *n;
 
@@ -472,17 +471,7 @@ static int fastrpc_mmap_find(struct fastrpc_file *fl, int fd, uintptr_t va,
 		return -EOVERFLOW;
 	if (mflags == ADSP_MMAP_HEAP_ADDR ||
 				 mflags == ADSP_MMAP_REMOTE_HEAP_ADDR) {
-		spin_lock(&me->hlock);
-		hlist_for_each_entry_safe(map, n, &me->maps, hn) {
-			if (va >= map->va &&
-				va + len <= map->va + map->len &&
-				map->fd == fd) {
-				map->refs++;
-				match = map;
-				break;
-			}
-		}
-		spin_unlock(&me->hlock);
+		return -ENOTTY;
 	} else {
 		spin_lock(&fl->hlock);
 		hlist_for_each_entry_safe(map, n, &fl->maps, hn) {
@@ -796,8 +785,9 @@ static int fastrpc_mmap_create(struct fastrpc_file *fl, int fd, unsigned attr,
 		map->va = va;
 	}
 	map->len = len;
-
-	fastrpc_mmap_add(map);
+	if ((mflags != ADSP_MMAP_HEAP_ADDR) &&
+			(mflags != ADSP_MMAP_REMOTE_HEAP_ADDR))
+		fastrpc_mmap_add(map);
 	*ppmap = map;
 
 bail:
@@ -1914,6 +1904,7 @@ static int fastrpc_init_process(struct fastrpc_file *fl,
 				 &mem));
 			if (err)
 				goto bail;
+			fastrpc_mmap_add_global(mem);
 			phys = mem->phys;
 			size = mem->size;
 			VERIFY(err, !hyp_assign_phys(phys, (uint64_t)size,
@@ -2200,7 +2191,7 @@ static int fastrpc_mmap_remove_ssr(struct fastrpc_file *fl)
 	} while (match);
 bail:
 	if (err && match)
-		fastrpc_mmap_add(match);
+		fastrpc_mmap_add_global(match);
 	return err;
 }
 
@@ -2249,8 +2240,13 @@ static int fastrpc_internal_munmap(struct fastrpc_file *fl,
 		goto bail;
 	fastrpc_mmap_free(map);
 bail:
-	if (err && map)
-		fastrpc_mmap_add(map);
+	if (err && map) {
+		if ((map->flags == ADSP_MMAP_HEAP_ADDR) ||
+				(map->flags == ADSP_MMAP_REMOTE_HEAP_ADDR))
+			fastrpc_mmap_add_global(map);
+		else
+			fastrpc_mmap_add(map);
+	}
 	mutex_unlock(&fl->map_mutex);
 	return err;
 }
@@ -2307,6 +2303,9 @@ static int fastrpc_internal_mmap(struct fastrpc_file *fl,
 		if (err)
 			goto bail;
 		map->raddr = raddr;
+		if (ud->flags == ADSP_MMAP_HEAP_ADDR ||
+				ud->flags == ADSP_MMAP_REMOTE_HEAP_ADDR)
+			fastrpc_mmap_add_global(map);
 	}
 	ud->vaddrout = raddr;
  bail:
